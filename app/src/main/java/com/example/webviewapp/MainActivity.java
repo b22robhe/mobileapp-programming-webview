615d409678abff472054a36b5145f8d3e10b2bdc
b22robhe <96245488+b22robhe@users.noreply.github.com>
2024-04-12 11:52:43 +0200

    Created webView private member variable assigned "my_webView" to this variable the added a new WebViewClient to the webView and imported WebView dependencies

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -5,11 +5,15 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView myWebView;
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
     }
@@ -25,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        myWebView = findViewById(R.id.my_webview);
+        myWebView.setWebViewClient(new WebViewClient());
         /*
         * Rename your App. Tip: Values->Strings
         * Enable Internet access for your App. Tip: Manifest
